731ce5b349c21e15b4a4bc88bba908fe47805c83
Deon B <deonsocode@gmail.com>
2023-02-06 21:25:50 -0700

    updated hamburger/ nav UI and transition

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+// this function below closes the nav window when one of the links are clicked on
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
